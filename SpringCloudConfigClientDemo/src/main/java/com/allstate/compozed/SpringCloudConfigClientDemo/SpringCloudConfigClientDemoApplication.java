4fbd3c5a09df1eda5c8944a5d163bd200a6cab87
Nathan Fitzgibbon <naththth@catmail.arizona.edu>
2017-12-09 15:37:27 -0700

    Add demo server and client

---
+++ SpringCloudConfigClientDemo/src/main/java/com/allstate/compozed/SpringCloudConfigClientDemo/SpringCloudConfigClientDemoApplication.java
@@ -0,0 +1,29 @@
+package com.allstate.compozed.SpringCloudConfigClientDemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringCloudConfigClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class DemoRestController {
+
+	@Value("${message:Hello World!}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage(){
+		return this.message;
+	}
+}
